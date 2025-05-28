ba763375fba7a2a61d2be984568794afa4773a3f
manuelfreirekgs <mfreirees@gmail.com>
2025-05-28 09:29:35 -0500

    Update datos001.php

--- datos001.php
+++ datos001.php
@@ -2,23 +2,18 @@
 //$host = 'dpg-d0r7293uibrs73cvc6kg-a.frankfurt-postgres.render.com';
 $host = 'dpg-d0r7293uibrs73cvc6kg-a';
 
-$db   = 'guivelly';
 $user = 'user';
 $pass = 'YuVYGYvGTu8oTJvLP6owctiFmq6pGZnx';
 $port = '5432';
 
-<script> console.log("manuel base 0"); </script>
 
 $conn = pg_connect("host=$host dbname=$db user=$user password=$pass port=$port");
 
 if (!$conn) {
   echo json_encode(['error' => 'No se pudo conectar a la base de datos']);
 
-  <script> console.log("manuel base 1"); </script>
   exit;
 }
-
-<script> console.log("manuel base 2"); </script>
 
 $query = "SELECT id, equipo, latitud, longitud, velocidad, fecha, hora FROM ubicaciones ORDER BY id DESC LIMIT 1";
 $result = pg_query($conn, $query);
