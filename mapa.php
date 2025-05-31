06085953351e7f6d4f4202f1b9192d5b94a1980b
manuelfreirekgs <mfreirees@gmail.com>
2025-05-31 09:18:19 -0500

    Update mapa.php

--- mapa.php
+++ mapa.php
@@ -12,52 +12,6 @@
     <h2>Ubicaciones en el mapa4</h2>
     <div id="map"></div>
 
-    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>GUIVELLY SPEED</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
-    <style>
-        #map { height: 500px; width: 100%; }
-    </style>
-</head>
-<body>
-    <h2>Ubicaciones en el mapa4</h2>
-    <div id="map"></div>
-
-    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>GUIVELLY SPEED</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
-    <style>
-        #map { height: 500px; width: 100%; }
-    </style>
-</head>
-<body>
-    <h2>Ubicaciones en el mapa4</h2>
-    <div id="map"></div>
-
-    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>GUIVELLY SPEED</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
-    <style>
-        #map { height: 500px; width: 100%; }
-    </style>
-</head>
-<body>
-    <h2>Ubicaciones en el mapa4</h2>
-    <div id="map"></div>
-
 <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 <script>
     console.log("Cargando mapa...");
@@ -89,19 +43,5 @@
             console.error("Error al obtener datos:", error);
         });
 </script>
-</body>
-</html>
-
-
-
-    
-</body>
-</html>
-
-    
-</body>
-</html>
-
-    
 </body>
 </html>
